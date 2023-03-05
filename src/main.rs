8149e598cc3c67ba6f36f077b8cddd97d5f166f7
iamtonmoy0 <tonmoy2code@gmail.com>
2023-03-05 18:42:17 -0500

    error fixed

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,11 @@
 
 use error_chain::error_chain;
-use std::{io::Read, string};
+use std::io::Read;
 error_chain!{
     foreign_links{
         Io(std::io::Error);
         HttpRequest(reqwest::Error);
-        
+
 
     }
 }
@@ -15,7 +15,7 @@ fn main()->Result<()>  {
    let mut body = String::new();
    res.read_to_string(&mut body)?;
    println!("Status :{}",res.status());
-   println!("Headers : \n{:#}",res.headers());
+   println!("Headers : \n{:#?}", res.headers());
    println!("Body:\n{}",body);
-   ok(())
+   Ok(())
 }
